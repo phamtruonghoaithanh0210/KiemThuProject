5c53d31f45b4fb820d5bc1a2aa85a6eed8f118aa
PhamTruongHoaiThanh0210 <1851050129thanh@ou.edu.vn>
2021-04-04 15:23:40 +0700

    add Jdbc connet to mysql

---
+++ Project/src/main/java/com/kiemthu/pojo/service/JdbcUtils.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kiemthu.pojo.service;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author acer
+ */
+public class JdbcUtils {
+        static{
+        try{
+            Class.forName("com.mysql.cj.jdbbc.Driver");
+        } catch (ClassNotFoundException ex) {
+           Logger.getLogger(JdbcUtils.class.getName()).log(Level.SEVERE,null,ex);
+        }
+    }
+    
+    public static Connection getconn() throws SQLException {
+        return DriverManager.getConnection("jdbc:mysql://localhost/saledb", "root", "1851050129");
+    }
+}
