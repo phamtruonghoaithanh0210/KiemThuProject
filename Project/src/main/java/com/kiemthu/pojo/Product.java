9f0d782a6194187a610595343f0e4cf68b0eec1b
PhamTruongHoaiThanh0210 <1851050129thanh@ou.edu.vn>
2021-04-04 19:28:43 +0700

    updated Class Product

---
+++ Project/src/main/java/com/kiemthu/pojo/Product.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kiemthu.pojo;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author acer
+ */
+public class Product {
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the price
+     */
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return the categoryid
+     */
+    public int getCategoryid() {
+        return categoryid;
+    }
+
+    /**
+     * @param categoryid the categoryid to set
+     */
+    public void setCategoryid(int categoryid) {
+        this.categoryid = categoryid;
+    }
+    private int id;
+    private String name;
+    private BigDecimal price;
+    private String description;
+    private int categoryid;
+}
