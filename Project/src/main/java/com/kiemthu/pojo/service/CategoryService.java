31d4a2770b51b3f54defeef16d96a053448ed7ef
PhamTruongHoaiThanh0210 <1851050129thanh@ou.edu.vn>
2021-04-04 19:29:24 +0700

    updated class CategoryService

--- Project/src/main/java/com/kiemthu/pojo/service/CategoryService.java
+++ Project/src/main/java/com/kiemthu/pojo/service/CategoryService.java
@@ -1,14 +1,40 @@
+package com.kiemthu.pojo.service;
+
+import com.kiemthu.pojo.Category;
+import com.kiemthu.pojo.service.JdbcUtils;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- */
-package com.kiemthu.pojo.service;
+/
+
 
 /**
  *
  * @author acer
  */
 public class CategoryService {
-    
+    public List<Category> getCates() throws SQLException {
+        Connection conn = JdbcUtils.getconn();
+        Statement stm = conn.createStatement();
+        ResultSet rs = stm.executeQuery("SELECT * FROM category");
+        
+        List<Category> cates = new ArrayList<>();
+        while (rs.next()) {
+            Category c = new Category();
+            c.setId(rs.getInt("id"));
+            c.setName(rs.getString("name"));
+            cates.add(c);
+        }
+        
+        conn.close();
+        return cates;
+    }
 }
