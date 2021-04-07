2ae924b9a4ff881dcbec608a04e02da5ba323ebc
vothikimyen108 <vothikinyen2k@gmail.com>
2021-04-07 22:45:30 +0700

    code funtioc getcustomer

--- Project/src/main/java/com/kiemthu/pojo/service/CustomerService.java
+++ Project/src/main/java/com/kiemthu/pojo/service/CustomerService.java
@@ -5,8 +5,7 @@
  */
 package com.kiemthu.pojo.service;
 
-import com.kiemthu.pojo.Category;
-import com.kiemthu.pojo.Product;
+import com.kiemthu.pojo.Customer;
 import com.kiemthu.pojo.Staff;
 import com.kiemthu.pojo.User;
 import java.sql.Connection;
@@ -21,15 +20,15 @@ import java.util.List;
  *
  * @author ASUS
  */
-public class StaffService {
-        public List<Staff> getStaffs() throws SQLException {
+public class CustomerService {
+    public List<Customer> getCustomer() throws SQLException {
         Connection conn = JdbcUtils.getconn();
         Statement stm = conn.createStatement();
-        ResultSet rs = stm.executeQuery("SELECT * FROM user where user_role like 'Staff' ");
-        List<Staff> staffs = new ArrayList<>();
+        ResultSet rs = stm.executeQuery("SELECT * FROM user where user_role like 'Customer' ");
+        List<Customer> Customers = new ArrayList<>();
         while (rs.next()) {
             // xư li bang nhan vien lay username password
-            Staff c = new Staff();
+            Customer c = new Customer();
             c.setIduser(rs.getInt("iduser"));
             c.setName(rs.getString("name"));
             c.setEmail(rs.getString("email"));
@@ -38,18 +37,10 @@ public class StaffService {
             c.setBirthday(rs.getDate("birthday"));
             c.setCreatDate(rs.getDate("create_date"));
             c.setPhone(rs.getString("phone"));
-            c.setUserRole(User.Role.Staff);
-            String s = "SELECT * FROM staff where idStaff =? ";
-            PreparedStatement pre = conn.prepareStatement(s);
-            pre.setInt(1, c.getIduser());
-            ResultSet rsStaff = pre.executeQuery();
-                while (rsStaff.next()) {
-                 c.setUsername(rsStaff.getString("username"));
-                 c.setPassword(rsStaff.getString("password"));
-            }
-            staffs .add(c);
+            c.setUserRole(User.Role.Customer);
+            Customers.add(c);
         }
         conn.close();
-        return staffs;
+        return Customers;
     }
 }
