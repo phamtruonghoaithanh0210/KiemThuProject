dea65af40ea9f79d370a234453d6613adcb01927
vothikimyen108 <vothikinyen2k@gmail.com>
2021-04-20 15:42:49 +0700

    code show staff detail

--- Project/src/main/java/com/kiemthu/project/StaffManegerController.java
+++ Project/src/main/java/com/kiemthu/project/StaffManegerController.java
@@ -13,6 +13,8 @@ import java.net.URL;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -21,16 +23,23 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.collections.transformation.SortedList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.control.TreeTableColumn.CellDataFeatures;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -64,6 +73,28 @@ public class StaffManegerController implements Initializable {
     private TextField filterField;
     @FXML
     private TableColumn<Staff, String> colusername;
+
+    @FXML
+    private TextField txtname;
+    @FXML
+    private TextField txtemail;
+    @FXML
+    private TextField txtphone;
+    @FXML
+    private TextField txtaddress;
+    @FXML
+    private TextField txtusername;
+    @FXML
+    private PasswordField txtpassword;
+    @FXML
+    private PasswordField txtcomfirmpassword;
+    @FXML
+    private RadioButton rdMale;
+    @FXML
+    private RadioButton rdfemale;
+    @FXML
+    private DatePicker idBirthday;
+    private ToggleGroup group = new ToggleGroup();
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -125,19 +156,54 @@ public class StaffManegerController implements Initializable {
         } catch (SQLException ex) {
             Logger.getLogger(StaffManegerController.class.getName()).log(Level.SEVERE, null, ex);
         }
+        tablestaff.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if (event.isPrimaryButtonDown() && event.getClickCount() == 2) {
+                    changeStaffDetail();
+                }
+            }
+        });
 
     }
 
     public void addStaff() throws IOException {
-    try {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("addstaff.fxml"));
-        Parent root1 = (Parent) fxmlLoader.load();
-        Stage stage = new Stage();
-        stage.setScene(new Scene(root1));  
-        stage.show();
-    } catch(Exception e) {
-        e.printStackTrace();
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("addstaff.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
+    public LocalDate convertToEntityAttribute(Date date) {
+        return Optional.ofNullable(date)
+                .map(Date::toLocalDate)
+                .orElse(null);
     }
+
+    public Date convertToDatabaseColumn(LocalDate localDate) {
+        return Optional.ofNullable(localDate)
+                .map(Date::valueOf)
+                .orElse(null);
+    }
+    //showw bảng detail
+    public void changeStaffDetail() {
+        Staff Staffselected = tablestaff.getSelectionModel().getSelectedItem();
+        txtname.setText(Staffselected.getName());
+        txtaddress.setText(Staffselected.getAddress());
+        txtemail.setText(Staffselected.getEmail());
+        txtphone.setText(Staffselected.getPhone());
+        idBirthday.setValue(this.convertToEntityAttribute(Staffselected.getBirthday()));
+        if (Staffselected.isGender()) {
+            rdMale.setSelected(true);
+        } else {
+            rdfemale.setSelected(true);
+        }
+
+    }
+    
 }
