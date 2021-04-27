b389cd3e66c4cbd001f247e9e6a284b242247b1e
PhamTruongHoaiThanh0210 <1851050129thanh@ou.edu.vn>
2021-04-28 00:46:59 +0700

    RTest

--- Project/src/test/java/Service/ReceiptTester.java
+++ Project/src/test/java/Service/ReceiptTester.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -43,7 +43,7 @@ public class ReceiptTester {
             ReceiptService re = new ReceiptService();
             Assertions.assertTrue(re.addReceipt(r));
         } catch (SQLException ex) {
-            Logger.getLogger(CategoryTester.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(ReceiptTester.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
@@ -59,7 +59,7 @@ public class ReceiptTester {
             ReceiptService re = new ReceiptService();
             Assertions.assertFalse(re.addReceipt(r));
         } catch (SQLException ex) {
-            Logger.getLogger(CategoryTester.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(ReceiptTester.class.getName()).log(Level.SEVERE, null, ex);
         }
     } 
     
@@ -80,52 +80,3 @@ public class ReceiptTester {
         Assertions.assertFalse(receipt.size()== 2);
     }
 }
-=======
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package Service;
-//
-//import com.kiemthu.pojo.Receipt;
-//import com.kiemthu.pojo.service.ReceiptService;
-//import java.sql.Connection;
-//import java.sql.Date;
-//import java.sql.SQLException;
-//import java.time.LocalDate;
-//import java.util.List;
-//import java.util.logging.Level;
-//import java.util.logging.Logger;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.Assertions;
-//
-///**
-// *
-// * @author acer
-// */
-//public class ReceiptTester {
-//    private Connection conn;
-//    @Test
-//    public void testQuantity() throws SQLException {
-//        List<Receipt> reces  = new ReceiptService().getReceipt();
-//        Assertions.assertTrue(reces.size() >= 1);
-//    }
-//    
-//    @Test
-//    public void testAddReceipt(){
-//        try {
-//            Receipt r = new Receipt();
-//            r.setCreateDate(Date.valueOf(LocalDate.MAX));
-//            r.setTotal(new Float(180000));
-//            r.setCustomer_id(2);
-//            r.setStaff_id(1);
-//            
-//            ReceiptService re = new ReceiptService();
-//            Assertions.assertTrue(re.addReceipt(r));
-//        } catch (SQLException ex) {
-//            Logger.getLogger(CategoryTester.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//    }
-//}
->>>>>>> 794e59e11882b606a409c5c3ccbea85d065bdc7a
